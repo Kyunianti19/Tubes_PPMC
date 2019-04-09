6672cfac97402cc0baf9bc275d01dd01ef1297cd
aminulsolihin <38267704+aminulsolihin@users.noreply.github.com>
2019-04-09 14:41:26 +0700

    Update library.c

--- library.c
+++ library.c
@@ -546,61 +546,78 @@ void designRuleChecker(component part[3][5]){
 		FILE *drc;
 		drc = fopen("DRC.txt","r");
 		
-		int q=0;
+		
 		while(!feof(drc)) {
 			fgets(baca,100,drc);
+			puts(baca);
 			for (int i=0;i<strlen(baca);i++){
 				 if(baca[i] !=' '){
-					 arr[c-1] = malloc(sizeof((char)*c));
+					 arr[c-1] = malloc(sizeof(char)*c);
 					 arr[c-1][i] = baca[i];
+					
 				 }
 				 c++;
-				 arr[c-1] = realloc(arr[c-1], sizeof(char)*c);
+				 arr[c-1] = realloc(arr[c-1],sizeof(char)*c);
+				 
 			 }
 			 r++;
 			 arr = realloc(arr, sizeof(char)*r);
+			
 		}
-						
+		fclose(drc);
+							 
+		
+	
+		char cekT ={' '};
 		int max = 40;		
 		int designcek = 0;
 		int cekkoordinat = 0;
 		for (int i = 1; i<=3;i++){
-			for (int j =1;j<=max;j++){
+			for (int j =1;j<=5;j++){
 				for (int k = 1;k<=max;k++){
 					for (int l = 1;l<=2;l++){
 						for (int m = 1;m<=2;m++){
 							if (part[i-1][j].point[k-1].lokasi[l-1].kaki[m] == part[i][j].point[k].lokasi[l].kaki[m]){
 								cekkoordinat++;
+								if(part[i][j].point[k].lokasi[l].kaki[m] == T.B.kaki[m] ){
+									cekT = 'B';
+								}
+								else if (part[i][j].point[k].lokasi[l].kaki[m] == T.E.kaki[m] ){
+									cekT = 'E';
+								}
+								else if (part[i][j].point[k].lokasi[l].kaki[m] == T.C.kaki[m] ){
+									cekT = 'C';						
+								}
 							}
 							if (cekkoordinat ==2){							  
 								for (int n = 1;n<=max;n++){
 									if (arr[n][2] == arr[n-1][2] || arr[n][3] == arr[n-1][3]) {
-										if (arr[n][0] == 'C' && arr[n][1] == 'b' && i ==1 && j==1){
-											designcek++
+										if (arr[n][0] == 'C' && arr[n][1] == 'b' && i ==1 && j==1 && cekT =='B'){
+											designcek++;
 										}
-										else if (arr[n][0] == 'C' && arr[n][1] == 'c' && i ==1 && j==2){
-											designcek++
+										else if (arr[n][0] == 'C' && arr[n][1] == 'c' && i ==1 && j==2 && cekT =='C'){
+											designcek++;
 										}
-										else if (arr[n][0] == 'C' && arr[n][1] == 'e' &&i ==1 && j==3){
-											designcek++
+										else if (arr[n][0] == 'C' && arr[n][1] == 'e' && i ==1 && j==3 && cekT =='E'){
+											designcek++;
 										}
-										else if (arr[n][0] == 'R' && arr[n][1] == '1' && i ==2 && j==1){
-											designcek++
+										else if (arr[n][0] == 'R' && arr[n][1] == '1' && i ==2 && j==1 && cekT =='B'){
+											designcek++;
 										}
-										else if (arr[n][0] == 'R' && arr[n][1] == '2' && i ==2 && j==2){
-											designcek++
+										else if (arr[n][0] == 'R' && arr[n][1] == '2' && i ==2 && j==2 && cekT =='B'){
+											designcek++;
 										}
-										else if (arr[n][0] == 'R' && arr[n][1] == 'c' &&i ==2 && j==3){
-											designcek++
+										else if (arr[n][0] == 'R' && arr[n][1] == 'c' &&i ==2 && j==3 && cekT =='C'){
+											designcek++;
 										}
-										else if (arr[n][0] == 'R' && arr[n][1] == 'e' &&i ==2 && j==4){
-											designcek++
+										else if (arr[n][0] == 'R' && arr[n][1] == 'e' &&i ==2 && j==4 && cekT =='E'){
+											designcek++;
 										}
 										else if (arr[n][0] == 'J' && arr[n][1] == '1' && i ==3 && j==1){
-											designcek++
+											designcek++;
 										}
-										else if (arr[n][0] == 'J' && arr[n][1] == '2' && i ==3 && j==2){
-											designcek++
+										else if (arr[n][0] == 'J' && arr[n][1] == '2' && i ==3 && j==2 && cekT =='C'){
+											designcek++;
 										}
 										else {
 											if( i ==1){
@@ -625,6 +642,7 @@ void designRuleChecker(component part[3][5]){
 												}
 												else if (j==4){
 													printf("ERROR! Kaki %c terhubung dengan Kaki %s ",arr[n][0],"Re");
+												}
 											else if(i==3){
 												if (j==1){
 													printf("ERROR! Kaki %c terhubung dengan Kaki %s ",arr[n][0],"Ji");
@@ -638,40 +656,41 @@ void designRuleChecker(component part[3][5]){
 									}
 								}
 							}
-						}
-					}
+						
+					
+				
 			else {
 				if( i ==1){
 					if (j==1){
-						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[n][0],"Cb");
+						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[j][0],"Cb");
 						}
 					else if (j==2){
-						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[n][0],"Cc");
+						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[j][0],"Cc");
 						}
 					else if (j==3){
-						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[n][0],"Ce");
+						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[j][0],"Ce");
 						}
 					}
 				else if(i==2){
 					if (j==1){
-						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[n][0],"R1");
+						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[j][0],"R1");
 						}
 					else if (j==2){
-						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[n][0],"R2");
+						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[j][0],"R2");
 						}
 					else if (j==3){
-						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[n][0],"Rc");
+						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[j][0],"Rc");
 						}
 					else if (j==4){
-						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[n][0],"Re");
+						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[j][0],"Re");
 					}
 					}
 				else if(i==3){
 					if (j==1){
-						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[n][0],"Ji");
+						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[j][0],"Ji");
 						}
 					else if (j==2){
-						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[n][0],"Jo");
+						printf("ERROR! Kaki %c tidak terhubung dengan Kaki %s ",arr[j][0],"Jo");
 					}
 					}
 					}
@@ -682,6 +701,8 @@ void designRuleChecker(component part[3][5]){
 	}
 }
 }
+}
+
 		
 								
 
